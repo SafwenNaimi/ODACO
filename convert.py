61a57e9b0c843b39938b607057250074b5005c14
Safwen <safwen.naimi@gmail.com>
2020-08-12 13:53:12 +0100

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,40 +1,36 @@
-import datetime
-import os, sys, shutil
+import cv2
 import numpy as np
-from numpy import loadtxt
-import pandas as pd
-from tqdm import tqdm
-import matplotlib.pyplot as plt
-from PIL import Image
-from sklearn.metrics import confusion_matrix, cohen_kappa_score
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
-from tensorflow.keras import optimizers, applications
-from tensorflow.keras.applications import VGG19
-from tensorflow.keras import layers
-from tensorflow.keras.layers import Dense, Dropout, GlobalAveragePooling2D, Input
-from tensorflow.keras import models
-from tensorflow.keras.models import Model,Sequential
-from tensorflow.keras.models import load_model
-from tensorflow.keras import optimizers
-from tensorflow.keras.applications import ResNet50
-from tensorflow.keras.callbacks import TensorBoard
+import imutils
+import sys
 import time
-import tensorflow as tf
-from keras.utils import CustomObjectScope
-from keras.initializers import glorot_uniform
+
+keyboard = np.zeros((1000, 1000, 3), np.uint8)
+
+image_0=cv2.imread('postures-de-yoga8.jpg')
+image_1=cv2.imread('a1c78be1f0ba682b2a8a9df496be33d7.jpg')
+
+#print(image_0.shape)
+#print(image_1.shape)
+
+key=np.vstack([image_0,image_1])
+#print(key.shape)
+#print(keyboard.shape)
+keyboard = cv2.resize(keyboard, (2500, 1190))
+final=np.hstack([key,keyboard])
+final = imutils.resize(final, height=600)
+cv2.putText(final,"Levez les bras et le torse d une seule respiration. ",(500,150),1,2,(255,0,0),2)
+cv2.putText(final,"Le pied arriere devrait marquer un angle de 90 degres. ",(500,200),1,2,(255,0,0),2)
+cv2.putText(final,"la cuisse avant devrait etre parallele au sol. ",(500,250),1,2,(255,0,0),2)
+cv2.putText(final,"l avant du genou directement au-dessus de la cheville. ",(500,300),1,2,(255,0,0),2)
+cv2.putText(final,"Reprenez la posture de l autre cote du corps. ",(500,350),1,2,(255,0,0),2)
+
+cv2.imshow("Result",final)
+#cv2.imwrite("hh.png",final)
+cv2.waitKey(0)
 
 
-
-
-model=tf.keras.models.load_model("Xception_model/mod.h5")#,custom_objects={'PruneLowMagnitude': prune_low_magnitude_output()})
-converter = tf.lite.TFLiteConverter.from_keras_model(model)
-converter.optimizations = [tf.lite.Optimize.DEFAULT]#POST-Trainig quantization (default:converter wimm try to figure out the best balance between size and latency)
-                                        #.OPTIMIZE_FOR_SIZE (optimize for size)
-                                        #.OPTIMIZE_FOR_LATENCY (optimize for latency) Latency is a networking term to describe the total time it takes a
-                                                                                    #data packet to travel from one node to another(temps de transmission)
-#converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-tflite_model=converter.convert()
-tflite_model_file='converted_modell_Xc.tflite'
-with open(tflite_model_file,"wb") as f:
-    f.write(tflite_model)
+for i in range(0,5):
+    
+    sys.stdout.write(str(i)+' ')
+    sys.stdout.flush()
+    time.sleep(1)
